ee5b807a0628aec4cf38bb4b1261a81fad47543b
camillynhacode <camynunespatricio@gmail.com>
2022-01-29 22:28:52 -0300

    removido permissão para criar usuário

--- usuarios/api/viewsets.py
+++ usuarios/api/viewsets.py
@@ -1,6 +1,4 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets, mixins
 
 from usuarios.models import Usuario
@@ -8,5 +6,8 @@ from usuarios.models import Usuario
 from .serializers import UsuarioSerializer
 
 class UsuarioviewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = (AllowAny,)
     queryset=Usuario.objects.all()
     serializer_class=UsuarioSerializer
+
+
