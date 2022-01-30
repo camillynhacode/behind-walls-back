76287929303d147421f46fc4227d2aa1f7c28865
camillynhacode <camynunespatricio@gmail.com>
2022-01-29 22:14:49 -0300

    adicionado rota de criação de usuario

--- behindwalls/urls.py
+++ behindwalls/urls.py
@@ -14,15 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from rest_framework import routers
+
+from usuarios.api.viewsets import UsuarioviewSet
+
+router=routers.DefaultRouter()
+router.register(r"usuarios", UsuarioviewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- usuarios/api/viewsets.py
+++ usuarios/api/viewsets.py
@@ -1,17 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, mixins
 
 from usuarios.models import Usuario
 
 from .serializers import UsuarioSerializer
 
-class UsuarioviewSet(ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+class UsuarioviewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset=Usuario.objects.all()
     serializer_class=UsuarioSerializer
-
-    def retrive(self, request):
-        queryset = self.queryset.filter(usuario=request.user)
-        serializer = UsuarioSerializer(queryset, many=True)
-        return Response(serializer.data)
